c524cb5346189c6afaea1c07c295e3817d8f22d0
Eliezer Soares <eliezerdrum@gmail.com>
2020-09-08 19:52:37 -0300

    Web Application

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,22 +3,29 @@ import './styles.css';
 
 import logo from '../../assets/logo.png';
 
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <div id="page-home">
       <div className="content">
         <header className="header">
-          <img src={logo} alt="logo" />
-            <nav id="menu">
-              <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Cadastre-se</a></li>
-                <li><a href="#">Sobre nós</a></li>
-                <li><a href="#">Contato</a></li>
-              </ul>
+            <img src={logo} alt="logo" />
+
+            <nav className="menu">
+              <Link to="/">
+               <span>Início</span> 
+            </Link>
+              <Link to="/">
+              <span>Serviços</span> 
+            </Link>
+              <Link to="/">
+              <span>Sobre nós</span> 
+            </Link>
+              <Link to="/">
+              <span>Contato</span> 
+            </Link>
             </nav>
-
-
         </header>
 
         <main>
@@ -26,9 +33,9 @@ const Home = () => {
           <p>Ajudamos você trazendo comodidade em suas compras</p>
         </main>
 
-      <div className="footer">
-        <p>Desenvolvido por Eliezer</p>
-      </div>
+        <div className="footer">
+          <p>Desenvolvido por Eliezer</p>
+        </div>
       </div>
     </div>
   );
