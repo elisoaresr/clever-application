97b819c01c7877b7e0ff4793fe18b7df39029ca0
Eliezer Soares <eliezerdrum@gmail.com>
2020-09-12 15:57:27 -0300

    Web Application

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -11,16 +11,14 @@ const Home = () => {
           <nav className="c2">
             <ul className="menu clearfix">
               <li><a href="#">Empresa</a>
-             Nível 1
-             submenu
-            <ul className="sub-menu clearfix">
+                <ul className="sub-menu clearfix">
                   <li><a href="#">Quem somos</a></li>
                   <li><a href="#">Como o clever Funciona?</a></li>
                   <li><a href="#">Blog</a></li>
                   <li><a href="#">Carreiras</a></li>
                   <li><a href="#">Investidores</a></li>
                 </ul> submenu
-            </li>
+              </li>
               <li><a href="#">Segurança</a></li>
               <li><a href="#">Ajuda</a></li>
               <li><a href="#">COVID-19 (Recursos)</a></li>
